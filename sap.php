4abc6b816984f5d6eca5082bde357a1e5ca5c62e
Thiago Lucena <thiago@teraidc.com.br>
2021-04-01 17:14:26 -0300

    Lista PNs

--- sap.php
+++ sap.php
@@ -11,6 +11,7 @@ class SAP{
         $cookie='cookie.txt';
         $data_arq=date("U",filemtime($cookie));
         $diff=time()-$data_arq;
+        $url=SAP_URL."/Login";
         //Faco a verificacao se o tempo de expiracao da sessao esta proximo, ou se esta sendo forcado o login
         if($diff>1000 || $force=='force'){
 
@@ -22,7 +23,7 @@ class SAP{
             $post=json_encode($dados);
 
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, SAP_URL);
+            curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -35,5 +36,27 @@ class SAP{
 
             return $json;
         }
+    }
+
+    #Parceiros de Negocios
+    /*
+    Listo os Parceiros de Negócios
+    */
+    public function getPN(){
+        header('Content-type: application/json');
+        $login=$this->login();
+        $url=SAP_URL."/BusinessPartners";
+        $cookie='cookie.txt';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie); //Envio o Cookie armazenado no Login
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $json = curl_exec($ch);
+        curl_close($ch);
+
+        return $json;
     }
 }
